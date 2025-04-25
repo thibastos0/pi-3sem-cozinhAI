1e77c36ab9c7cc913dd8ff52af49d7690deab207
ericmeneg <ericvinicius.menegon@gmail.com>
2025-04-25 12:06:07 -0300

    Iniciando o projeto do next

---
+++ cozinhai-next/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <div className="header">
+      <h1 className="flex items-center justify-center mt-5 font-bold text-4xl">CozinhAI</h1>
+    </div>
+    </>
+  );
+}
