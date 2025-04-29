13f79afb3a9b53e2bf7f5e9a59744fc10adbbffb
ericmeneg <ericvinicius.menegon@gmail.com>
2025-04-29 08:43:28 -0300

    Adicionando a font-family DaCherry ao globals.css para uso no projeto

--- cozinhai-next/src/app/page.tsx
+++ cozinhai-next/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
     <div className="header">
-      <h1 className="flex items-center justify-center mt-5 font-bold text-4xl">CozinhAI</h1>
+      <h1 className="flex items-center justify-center mt-5 font-bold text-4xl" style= {{fontFamily: 'DaCherry'}} >CozinhAI</h1>
     </div>
     </>
   );
