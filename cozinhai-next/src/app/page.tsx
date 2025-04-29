06c24adfd0acdac8f8513f07aed32f75b048899e
ericmeneg <ericvinicius.menegon@gmail.com>
2025-04-29 12:38:50 -0300

    Adicionando a pasta images e melhorando o header da landing page

--- cozinhai-next/src/app/page.tsx
+++ cozinhai-next/src/app/page.tsx
@@ -3,9 +3,19 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <div className="header">
-      <h1 className="flex items-center justify-center mt-5 font-bold text-4xl" style= {{fontFamily: 'DaCherry'}} >CozinhAI</h1>
-    </div>
+      <div className="flex items-center justify-between px-8 py-4 bg-[#EDF2F4]">
+        <div className="flex items-center space-x-4">
+          <img src="/images/logo.png" alt="Logo da CozinhAi" className="h-12 w-auto" />
+          <h1 className="text-4xl font-bold" style={{ fontFamily: 'DaCherry' }}>
+            CozinhAI
+          </h1>
+        </div>
+
+        <button className="bg-gradient-to-r from-green-400 to-teal-500 text-white px-6 py-2 rounded-full shadow-md hover:brightness-110 transition">
+          Experimente agora!
+        </button>
+      </div>
+
     </>
   );
 }
