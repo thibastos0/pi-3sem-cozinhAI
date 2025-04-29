5c146327b625960ffaad53108a122683117a3e43
GRAZIELLA SOUZA <1051392411017@fatec>
2025-04-29 12:55:24 -0300

    Começando Home page e ajustando CSS

---
+++ cozinhai-next/src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return(
+        <div className="bg-[#EDF2F4] h-0.1">
+            <img className="m-2" src="/images/menuHeader.svg" />
+        </div>
+    )
+}
