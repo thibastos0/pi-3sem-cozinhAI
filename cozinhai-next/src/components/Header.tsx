9fc4bd54065fb73c46b4239fcabbac2504c1dae7
Graziella Souza <graziisouza0127@gmail.com>
2025-05-05 12:28:57 -0300

    Estilizando o cabeçalho com novos estilos de entrada

--- cozinhai-next/src/components/Header.tsx
+++ cozinhai-next/src/components/Header.tsx
@@ -1,7 +1,34 @@
+import styles from "../app/styles/home.module.css";
+
 export default function Header() {
-    return(
-        <div className="bg-[#EDF2F4] h-0.1">
-            <img className="m-2" src="/images/menuHeader.svg" />
+  return (
+    <div className="bg-[#EDF2F4] flex w-full">
+      <header
+        className={`flex flex-row items-center gap-7 justify-between w-full ${styles.header}`}
+      >
+        <button>
+          <img className="" src="/images/menuHeader.svg" />
+        </button>
+
+        <div className={styles.inputContainer}>
+          <input
+            className={`border-[#22577A] rounded-xl text-black border-2 p-1 font-alexandria w-lg ${styles.input}`}
+            type="text"
+            placeholder="Buscar"
+          />
+          <button>
+            <img
+              className={styles.lupaIcon}
+              src="/images/lupaIcon.svg"
+              alt=""
+            />
+          </button>
         </div>
-    )
+
+        <button>
+          <img className="w-7.5" src="/images/profileIcon.svg" alt="" />
+        </button>
+      </header>
+    </div>
+  );
 }
