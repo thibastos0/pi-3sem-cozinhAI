e733e66896b5e0d6cd46b4d8e64066b4125ecfd0
Graziella Souza <graziisouza0127@gmail.com>
2025-05-06 16:15:10 -0300

    Alinhando imagens dos cards de recomendação

--- cozinhai-next/src/components/RecomendacaoCard.tsx
+++ cozinhai-next/src/components/RecomendacaoCard.tsx
@@ -1,21 +1,25 @@
-import styles from '../app/styles/home.module.css'
-import Botao from './Botao';
+import styles from "../app/styles/home.module.css";
+import Botao from "./Botao";
 
-type CardProps = {
-    image: any;
-    title: string;
-}
+export type CardProps = {
+  image: any;
+  title: string;
+};
 
-export default function RecomendacaoCard({image, title}: CardProps) {
-    return(
-    <div className={` bg-[#FFFFFF] border-[#22577A] border-2 rounded-4xl flex items-center justify-center w-1/4 ${styles.card}`}>
-        <div className='flex flex-col gap-2'>
-            <div className={`${styles.imgContainer}`}>
-                <img src={image} className='rounded-[26]' />
-            </div>
-            <h2 className="font-semibold text-[#22577A] text-[22px]">{title}</h2>
-            <Botao texto='Ver Receita' destino='*' />
+export default function RecomendacaoCard({ image, title }: CardProps) {
+  return (
+    <div
+      className={` bg-[#FFFFFF] border-[#22577A] border-2 rounded-3xl flex items-center justify-center w-1/4 ${styles.card}`}
+    >
+      <div className="flex flex-col gap-2 justify-start">
+        <div className={`${styles.imgContainer}`}>
+          <img src={image} className="rounded-[22px]" />
         </div>
+        <h2 className="font-semibold text-[#22577A] text-[22px] min-h-17">
+          {title}
+        </h2>
+        <Botao texto="Ver Receita" destino="*" />
+      </div>
     </div>
-    )
+  );
 }
