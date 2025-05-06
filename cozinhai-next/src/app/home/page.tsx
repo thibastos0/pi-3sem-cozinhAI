16b39e8561ad28acd050a35cf088be5f837dda4e
Graziella <graziisouza0127@gmail.com>
2025-05-06 12:54:22 -0300

    Finalizando seção de Recomendações diárias

--- cozinhai-next/src/app/home/page.tsx
+++ cozinhai-next/src/app/home/page.tsx
@@ -1,11 +1,27 @@
 import Header from "@/components/Header";
-import Recomendacoes from "@/components/Recomendacoes";
+import RecomendacaoCard from "@/components/RecomendacaoCard";
+import styles from '../styles/home.module.css'
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-[#FFFFFF] w-dvw h-dvh">
       <Header />
-      <Recomendacoes />
+
+      {/* Seção de recomendações diárias */}
+      <main
+        className={`items-center justify-center flex flex-col gap-20 ${styles.main}`}
+      >
+        <img src="/images/fullLogo.svg" alt="" />
+        <h1 className="text-[#22577A] font-bold text-2xl">
+          Recomendações Diárias
+        </h1>
+
+        <div className="flex flex-row justify-around">
+          <RecomendacaoCard image="images/miniChurros.svg" title="Mini Churros"/>
+          <RecomendacaoCard image="images/panquecaDeBanana.svg" title="Panqueca de Banana"/>
+          <RecomendacaoCard image="images/boloDeCaneca.svg" title="Bolo de Caneca"/>
+        </div>
+      </main>
     </div>
   );
 }
--- cozinhai-next/src/components/Botao.tsx
+++ cozinhai-next/src/components/Botao.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
+import styles from '../app/styles/home.module.css'
 
 interface botaoProps {
     texto: string,
@@ -8,6 +9,11 @@ interface botaoProps {
 
 export default function Botao({ texto, destino }: botaoProps){
     return(
-        <Link className="bg-gradient-to-t from-[#22577A] to-[#57CC99] text-white px-6 py-2 rounded-full shadow-md hover:brightness-110 transition" style={{ padding: '0.5rem' }} href={destino}>{texto}</Link>
+        <Link className="bg-gradient-to-t from-[#22577A] to-[#57CC99] px-6 py-2 rounded-full shadow-md hover:brightness-110 transition" style={{ padding: '0.5rem' }} href={destino}>
+        <div className={`text-white flex flex-row justify-between ${styles.buttonContent}`}>
+            <p>{texto}</p>
+            <p>{'>'}</p>
+        </div>
+        </Link>
     )
 }
