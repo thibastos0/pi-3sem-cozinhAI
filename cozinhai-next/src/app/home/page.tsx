f311dfc0d9f590f94234f98b664258cf9c0abc36
Graziella Souza <graziisouza0127@gmail.com>
2025-05-06 08:37:51 -0300

    Iniciando página de recomendações

--- cozinhai-next/src/app/home/page.tsx
+++ cozinhai-next/src/app/home/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/Header";
+import Recomendacoes from "@/components/Recomendacoes";
 
 export default function Home() {
-    return(
-        <div>
-            <Header />
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <Recomendacoes />
+    </div>
+  );
 }
