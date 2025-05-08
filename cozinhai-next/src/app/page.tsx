94adee8a3bbe3ec9463341dfe8a9ec72b16b84d4
ericmeneg <ericvincius.menegon@gmail.com>
2025-05-08 11:07:45 -0300

    Adicionando imagem para a landing page, modificando o componente Botao para permitir o recebimento de fontSize

--- cozinhai-next/src/app/page.tsx
+++ cozinhai-next/src/app/page.tsx
@@ -1,10 +1,9 @@
 import Botao from "@/components/Botao";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <div className="flex items-center justify-between px-8 py-4 bg-[#EDF2F4] padding-x" style={{ padding: '1rem 5rem'  }}>
+      <div className="flex items-center justify-between px-8 py-4 bg-[#EDF2F4] padding-x" style={{ height: '9.063rem', padding: '1rem 5rem'  }}>
         <div className="flex items-center space-x-4">
           <img src="/images/logo.svg" alt="Logo da CozinhAi" className="h-12 w-auto" />
           <h1 className="text-4xl font-bold bg-gradient-to-t from-[#22577A] to-[#57CC99] bg-clip-text text-transparent" style={{ fontFamily: 'DaCherry', cursor: 'default', marginLeft: '0.4rem'}}>
@@ -16,8 +15,11 @@ export default function Home() {
       </div>
 
       <div>
-        <h1 className="bold" style={{fontFamily: 'Alexandria'}}>Lorem Ipsum!</h1>
+        <img alt="Bowl de salada com milho, tomate, ovos, e frango" src="/images/salada.svg"/>
       </div>
+      <h1 className="bold" style={{fontFamily: 'Alexandria', fontSize: "63.12", fontWeight: "bold"}}>Lorem Ipsum!</h1>
+      <p style={{fontSize: "31.56", fontWeight: "300"}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been </p>
+      <Botao texto="Experimente agora!" destino="/home" fontSize="28.92"/>
     </>
   );
 }
--- cozinhai-next/src/components/Botao.tsx
+++ cozinhai-next/src/components/Botao.tsx
@@ -5,9 +5,10 @@ import styles from "../app/styles/home.module.css";
 interface botaoProps {
   texto: string;
   destino: string;
+  fontSize?: string
 }
 
-export default function Botao({ texto, destino }: botaoProps) {
+export default function Botao({ texto, destino, fontSize }: botaoProps) {
   return (
     <Link
       className="bg-gradient-to-t from-[#22577A] to-[#57CC99] px-6 py-2 rounded-full shadow-md hover:brightness-110 transition"
@@ -16,6 +17,7 @@ export default function Botao({ texto, destino }: botaoProps) {
     >
       <div
         className={`text-white flex flex-row justify-between gap-2 ${styles.buttonContent}`}
+        style={fontSize ? { fontSize } : undefined}
       >
         <p>{texto}</p>
         <p>{">"}</p>
