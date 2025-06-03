7f38c882b2329336ee381cdf7c096979d4ee3ca7
Graziella Souza <graziisouza0127@gmail.com>
2025-06-03 10:15:09 -0300

    Ajustando iamegm do recomendacaoCard

--- cozinhai-next/src/components/RecomendacaoCard.tsx
+++ cozinhai-next/src/components/RecomendacaoCard.tsx
@@ -3,34 +3,29 @@ import Botao from "./Botao";
 import Image from "next/image";
 
 export type CardProps = {
-    image?: string;
-    title: string;
-    slug: string;
+  image?: string;
+  title: string;
+  slug: string;
 };
 
 export default function RecomendacaoCard({ image, title, slug }: CardProps) {
-    return (
-        <div className={`bg-white border-[#22577A] border-2 rounded-3xl flex items-center justify-center w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4 ${styles.card}`}>
-            <div className="flex flex-col gap-2 w-full">
-                <Image
-                    src={image || "/placeholder.png"}
-                    alt={title}
-                    className="rounded-[22px] w-full object-cover"
-                    width={400}
-                    height={250}
-                    style={{ objectFit: "cover" }}
-                />
-                <Image
-                    src={image || "/placeholder.png"}
-                    alt={title}
-                    className="rounded-[22px] w-full object-cover"
-                    width={400}
-                    height={250}
-                    style={{ objectFit: "cover" }}
-                />
-            </div>
-            <h2 className="font-semibold text-[#22577A] text-lg md:text-[22px] min-h-17">{title}</h2>
-            <Botao texto="Ver Receita" destino={`/receita/${slug}`} />
-        </div>
-    );
+  return (
+    <div
+      className={`bg-white border-[#22577A] border-2 rounded-3xl flex flex-col justify-center w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4 ${styles.card}`}
+    >
+      <div className="flex flex-col gap-2 w-full">
+        <Image
+          src={image || "/placeholder.png"}
+          alt={title}
+          className="rounded-[22px] w-full object-cover"
+          width={900}
+          height={550}
+        />
+      </div>
+      <h2 className="font-semibold text-[#22577A] text-lg md:text-[22px] min-h-17">
+        {title}
+      </h2>
+      <Botao texto="Ver Receita" destino={`/receita/${slug}`} />
+    </div>
+  );
 }
